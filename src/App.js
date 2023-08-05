46a6e0dd50ea3befa607a17ee33b75c9a3c6a1ff
Ryo Yoshino <ryodragon0528@gmail.com>
2023-08-05 14:54:19 +0900

    feat: Navbarの作成

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
 import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const initialUrl = "https://pokeapi.co/api/v2/pokemon/";
@@ -34,11 +35,14 @@ function App() {
       {isLoading ? (
         <h1>ロード中...</h1>
       ) : (
-        <div className="pokemonCardContainer">
-          {pokemonData.map((pokemon, i) => {
-            return <Card pokemon={pokemon} />;
-          })}
-        </div>
+        <>
+          <Navbar />
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card pokemon={pokemon} />;
+            })}
+          </div>
+        </>
       )}
     </div>
   );
