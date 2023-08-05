a93c504108c15539d0879da9f64a2c346da8d1d2
Ryo Yoshino <ryodragon0528@gmail.com>
2023-08-05 15:54:32 +0900

    feat: 表示を切り替えるボタンを実装

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ import Card from "./components/Card/Card";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const initialUrl = "https://pokeapi.co/api/v2/pokemon/";
+  const initialUrl = "https://pokeapi.co/api/v2/pokemon?limit=21";
   const [isLoading, setIsLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [prevUrl, setPrevUrl] = useState("");
+  const [nextUrl, setNextUrl] = useState("");
 
   useEffect(() => {
     const getPokemonApi = async () => {
       // すべてのポケモンデータを取得
       const pokemonAllData = await getAllPokemon(initialUrl);
+
       // 各ポケモンの詳細なデータを取得
       loadPokemon(pokemonAllData.results);
 
+      setPrevUrl(pokemonAllData.previous);
+      setNextUrl(pokemonAllData.next);
       setIsLoading(false);
     };
     getPokemonApi();
@@ -25,10 +30,37 @@ function App() {
     const _pokemonDetailData = await Promise.all(
       results.map(async (pokemon) => await getPokemon(pokemon.url))
     );
+    console.log(_pokemonDetailData);
     setPokemonData(_pokemonDetailData);
   };
 
-  console.log(pokemonData);
+  const handlePrevPage = async () => {
+    if (!prevUrl) {
+      return;
+    }
+    setIsLoading(true);
+
+    const pokemonData = await getAllPokemon(prevUrl);
+    await loadPokemon(pokemonData.results);
+    setPrevUrl(pokemonData.previous);
+    setNextUrl(pokemonData.next);
+
+    setIsLoading(false);
+  };
+
+  const handleNextPage = async () => {
+    if (!nextUrl) {
+      return;
+    }
+    setIsLoading(true);
+
+    const pokemonData = await getAllPokemon(nextUrl);
+    await loadPokemon(pokemonData.results);
+    setPrevUrl(pokemonData.previous);
+    setNextUrl(pokemonData.next);
+
+    setIsLoading(false);
+  };
 
   return (
     <div className="App">
@@ -39,8 +71,16 @@ function App() {
           <Navbar />
           <div className="pokemonCardContainer">
             {pokemonData.map((pokemon, i) => {
-              return <Card pokemon={pokemon} />;
+              return <Card pokemon={pokemon} key={pokemon.name} />;
             })}
+          </div>
+          <div className="btn">
+            <button type="button" onClick={handlePrevPage}>
+              前へ
+            </button>
+            <button type="button" onClick={handleNextPage}>
+              次へ
+            </button>
           </div>
         </>
       )}
